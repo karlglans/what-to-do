9500bcf5066d118b7ea7285d4a5986fc6f8c1f72
karlglans <karl_glans@hotmail.com>
2020-02-11 18:45:09 +0100

    switching in a form-input

--- src/components/TodoList.tsx/List/ListItem.tsx
+++ src/components/TodoList.tsx/List/ListItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './list-item.css';
 
@@ -7,16 +7,39 @@ interface Props {
   id: number;
   ischecked: boolean;
   handleCheck(msgId: number): void;
-  removeNote(noteId: number): void
-  children: string;
+  removeNote(noteId: number): void;
+  editNote(noteId: number, message: string): void;
+  message: string;
+  setNoteToFocus(noteId: number): void;
+  isInFocus: boolean;
 }
 
-const ListItem: React.FC<Props> = ({ ischecked, id, children, handleCheck, removeNote}) => {
-  const [isShown, setIsShown] = useState(false);
-  function handleHover() {
+interface InputFiledProps {
+  text: string;
+  noteId: number;
+  editNote(noteId: number, message: string): void;
+  blureInput(): void;
+}
 
+const InputField: React.FC<InputFiledProps> = ({text, editNote, noteId, blureInput}) => {
+  function handleChange(event: any) {
+    editNote(noteId, event.target.value);
   }
+  // useEffect( ()=> {
+  //   console.log('InputField mounted')
+  // },[]);
+  return (<input value={text} onChange={handleChange} onBlurCapture={blureInput} className='todo-list-item-input'/>)
+};
+
+const ListItem: React.FC<Props> = ({ ischecked, id, message, handleCheck, removeNote, editNote, setNoteToFocus, isInFocus}) => {
+  const [isShown, setIsShown] = useState(false);
   const style = {display: 'flex', justifyContent: 'space-around', alignItems: 'center'};
+  function handleBlureInput() {
+    setNoteToFocus(-1);
+  }
+  function handleDoubleClick() {
+    setNoteToFocus(id);
+  }
   return (
     <li style={style} className='todo-list-item' onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
       <input type="checkbox"
@@ -26,8 +49,14 @@ const ListItem: React.FC<Props> = ({ ischecked, id, children, handleCheck, remov
         onChange={() => handleCheck(id)}
         >
         </input>
-      <span style={{flex: 10}}>{children}</span>
-      <button onClick={() => removeNote(id)} style={{flex: 0, visibility: !isShown ? 'hidden':'visible'}}>X</button>
+        <span style={{flex: 10}} onDoubleClick={handleDoubleClick}>
+          {isInFocus && (<InputField text={message} editNote={editNote} noteId={id} blureInput={handleBlureInput} />)}
+          {!isInFocus && (<>{message}</>)}
+        </span>
+      
+        {!isInFocus && (
+          <button onClick={() => removeNote(id)} style={{flex: 0, visibility: !isShown ? 'hidden':'visible'}}>X</button>
+        )}
     </li>
   );
 }
